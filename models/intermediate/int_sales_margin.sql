4ee9f4868a2d74de214cb3f3d854eb6750c49b30
Martha Huerta <marthah.112225@gmail.com>
2025-04-15 05:55:35 +0000

    finance folder

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,8 @@
+
+  SELECT
+      *,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
